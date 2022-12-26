54e532d3bc1ee78840397af3a446a2ed8001d89a
Yeon Joon Jung <yeonjoonjung1@gmail.com>
2022-12-26 11:19:50 -0800

    finished officers list functionality

--- scripts/officers-script.js
+++ scripts/officers-script.js
@@ -2,25 +2,27 @@
 * The purpose of doing in this approach is to automate the process of 
 * writing all KOJOBS officers in simpler and efficient manner. 
 */
+
 document.addEventListener("DOMContentLoaded", function() {
     load_representatives();
-    // load_hrs_team(); 
-    // load_pm_team(); 
-    // load_prd_team();
-    // load_md_team(); 
+    load_hrs_team(); 
+    load_pm_team(); 
+    load_prd_team();
+    load_md_team(); 
   });
 
+
 /* 
-* reads member data within representatives json file for 2022-23 year
-* and loads them to /officers.html page under its representatives box. 
+* fetches a given jsonfile, reads its data, then creates div element for each officer. 
+* we include officer's name, role, and their email to be displayed. 
+* Once the div element is created it appends the element to the officers_list element
+* passed down.
 * 
-* parameters: none
+* parameters: officers_list - document element 
+*             jsonfile - json file address
 * return value: none
 */
-function load_representatives() {
-    let officers_list = document.getElementById("executives-officers");
-
-    let jsonfile = './json/officers-22-23/representatives.json'; 
+function load_members(officers_list, jsonfile) {
     fetch(jsonfile)
         .then((response) => {
             return response.json();
@@ -37,7 +39,7 @@ function load_representatives() {
                 role.innerHTML += `Role: <b>${data.officers[i].role}</b>`; 
 
                 let email = document.createElement('div'); 
-                email.innerHTML += `Email: <b>${data.officers[i].role}</b>`; 
+                email.innerHTML += `Email: <b>${data.officers[i].email}</b>`; 
 
                 officer.appendChild(name);
                 officer.appendChild(role);
@@ -46,4 +48,78 @@ function load_representatives() {
                 officers_list.appendChild(officer); 
             }
         })
+} 
+
+/* 
+* reads member data within representatives json file for 2022-23 year
+* and loads them to /officers.html page within the div with id="representative-officers".
+* 
+* parameters: none
+* return value: none
+*/
+function load_representatives() {
+    let officers_list = document.getElementById("representative-officers");
+    let jsonfile = '../json/officers-22-23/representatives.json'; 
+      
+    load_members(officers_list, jsonfile); 
 }
+
+/* 
+* reads member data within human resources & statistics team json file for 2022-23 year
+* and loads them to /officers.html page within the div with id="hrs-officers".
+* 
+* parameters: none
+* return value: none
+*/
+function load_hrs_team() {
+    let officers_list = document.getElementById("hrs-officers");
+    let jsonfile = '../json/officers-22-23/hrs-team.json';  
+      
+    load_members(officers_list, jsonfile); 
+}
+
+/* 
+* reads member data within planning & management team json file for 2022-23 year
+* and loads them to /officers.html page within the div with id="pm-officers".
+* 
+* parameters: none
+* return value: none
+*/
+function load_pm_team() {
+    let officers_list = document.getElementById("pm-officers");
+    let jsonfile = '../json/officers-22-23/pm-team.json';  
+      
+    load_members(officers_list, jsonfile); 
+}
+
+/* 
+* reads member data within public relations & development team json file for 2022-23 year
+* and loads them to /officers.html page within the div with id="prd-officers".
+* 
+* parameters: none
+* return value: none
+*/
+function load_prd_team() {
+    let officers_list = document.getElementById("prd-officers");
+    let jsonfile = '../json/officers-22-23/prd-team.json';  
+      
+    load_members(officers_list, jsonfile); 
+}
+
+/* 
+* reads member data within marketing & design team json file for 2022-23 year
+* and loads them to /officers.html page within the div with id="md-officers". 
+* 
+* parameters: none
+* return value: none
+*/
+function load_md_team() {
+    let officers_list = document.getElementById("md-officers");
+    let jsonfile = '../json/officers-22-23/md-team.json';  
+      
+    load_members(officers_list, jsonfile); 
+}
+
+
+
+ 
